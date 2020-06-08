6cc4c5ffa86959f97eb764a6970a1d7a0597d6f1
melbeied <melbeied@gmail.com>
2020-06-08 09:43:22 +0100

    refactoring

--- terraform/03_compute/main.tf
+++ terraform/03_compute/main.tf
@@ -1,12 +1,12 @@
 module "sg" {
-    source = "./050_sg"
-    vpc_id      = var.vpc_id
-    vpc_cidr    = var.vpc_cidr
+    source          = "./050_sg"
+    vpc_id          = var.vpc_id
+    vpc_cidr        = var.vpc_cidr
 }
 module "ec2" {
     source          = "./051_ec2"
     vpc_id          = var.vpc_id
     aws_key_name    = var.aws_key_name
-    sg-public-id  = module.sg.sg-public-id
+    sg-public-id    = module.sg.sg-public-id
     sn-public-list  = var.sn-public-list
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -18,32 +18,29 @@ module "vpc" {
 }
 
 module "public-subnet" {
-    source = "./01_sn-public"
+    source = "./01_networking/010_sn-public"
     vpc_id      = module.vpc.id
 }
 module "front-subnet" {
-    source = "./02_sn-front"
+    source = "./01_networking/011_sn-front"
     vpc_id      = module.vpc.id
     list-region = module.public-subnet.used_az
     nat-gw-id   = module.public-subnet.ngw-id
 }
 module "back-subnet" {
-    source = "./03_sn-back"
+    source = "./01_networking/012_sn-back"
     vpc_id      = module.vpc.id
     list-region = module.public-subnet.used_az
     nat-gw-id   = module.public-subnet.ngw-id
 }
+# module "storage" {
+#     source = "./02_storage"
+#     vpc_id      = module.vpc.id
+# }
 module "compute" {
-    source = "./05_compute"
+    source = "./03_compute"
     vpc_id          = module.vpc.id
     vpc_cidr        = module.vpc.cidr
     aws_key_name    = var.aws_key_name
     sn-public-list  = module.public-subnet.public-sn-ids
-    #sg-public-ids   = tolist(["${module.public-subnet.public-sn-ids}"])
 }
-
-# module "networking" {
-#     source = "./04_network"
-#     vpc_id      = module.vpc.id
-#     public-subnet_ids = module.public-subnet.ids
-# }
