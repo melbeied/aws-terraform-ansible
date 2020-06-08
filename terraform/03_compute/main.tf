514a360972f439392923c3a2d1e5eb01e86d0f9e
melbeied <melbeied@gmail.com>
2020-06-08 09:46:47 +0100

    refactoring +

--- terraform/03_compute/main.tf
+++ terraform/03_compute/main.tf
@@ -1,10 +1,10 @@
 module "sg" {
-    source          = "./050_sg"
+    source          = "./030_sg"
     vpc_id          = var.vpc_id
     vpc_cidr        = var.vpc_cidr
 }
 module "ec2" {
-    source          = "./051_ec2"
+    source          = "./031_ec2"
     vpc_id          = var.vpc_id
     aws_key_name    = var.aws_key_name
     sg-public-id    = module.sg.sg-public-id
